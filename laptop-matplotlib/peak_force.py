ce33c1a47de7672f8656bb7fe3ea8ac6b52c29f0
Stu D <stu@sjmd.dev>
2021-08-20 21:06:04 +1200

    Save results.

--- laptop-matplotlib/peak_force.py
+++ laptop-matplotlib/peak_force.py
@@ -1,17 +1,23 @@
 from tindeq import TindeqProgressor
 from scipy import constants as cnst
 import matplotlib.animation as animation
-from matplotlib.widgets import Button
+from matplotlib.widgets import Button,TextBox
 import matplotlib.pyplot as plt
 from collections import deque
+from numpy import savetxt
 import asyncio
+from datetime import datetime
 import threading
+from os import path
 
 class PeakForce():
   units={'lb' : 1.0 / cnst.lb, 'kg' : 1.0 }
   def __init__(self, unit='kg'):
     if unit not in PeakForce.units:
       raise RuntimeError('unit=[\'kg\'|\'lb\']')
+    
+    self.basedir = path.expanduser('~') + "/Documents/hangboard/"
+    self.hproto="destiny" # hangboard or test protocol
     self.unit_scale = PeakForce.units[unit]
     self.unit_name = unit
 
@@ -20,28 +26,41 @@ class PeakForce():
     self.fig.canvas.mpl_connect('close_event', self.close)
     self.fig.canvas.set_window_title('Peak Force')
 
+    self.clear()
+
     plt.subplots_adjust(bottom=0.2)
-    self.aStart = plt.axes([0.7, 0.05, 0.1, 0.075])
-    self.bStart = Button(self.aStart, 'Start')
-    self.bStart.on_clicked(self.sstart)
+    self.aClear = plt.axes([0.7, 0.05, 0.1, 0.075])
+    self.bClear = Button(self.aClear, 'Clear')
+    self.bClear.on_clicked(self.sclear)
+    
     self.aStop = plt.axes([0.81, 0.05, 0.1, 0.075])
     self.bStop = Button(self.aStop, 'Stop')
     self.bStop.on_clicked(self.sstop)
-    
+
+    self.aResume = plt.axes([0.59, 0.05, 0.1, 0.075])
+    self.bResume = Button(self.aResume, 'Resume')
+    self.bResume.on_clicked(self.sresume)
+
+    self.aSave = plt.axes([0.48, 0.05, 0.1, 0.075])
+    self.bSave = Button(self.aSave, 'Save')
+    self.bSave.on_clicked(self.ssave)
+
+    self.aLabel = plt.axes([0.09, 0.05, 0.2, 0.075])
+    self.tLabel = TextBox(self.aLabel, "Hang:")
+  
+
     self.cStartThresh=4.0
     self.cStopThresh=2.0
     self.dq = deque()
-    self.plotx = []
-    self.ploty = []
-    self.plots = []
-    self.t0=[]
     self.tindeq = None
     self.statmsg = "Init..."
     self.running = True
-    self.collecting = False
+    self.collecting = True
     self.above_thresh = False
 
   def close(self, e):
+    if len(self.plotx) > 0:
+      self.ssave()
     self.collecting = False
     self.running = False
 
@@ -57,6 +76,7 @@ class PeakForce():
           self.plotx.append([])
           self.ploty.append([])
           self.t0.append(pt[0])
+          self.labels.append(self.tLabel.text)
         self.above_thresh = True
       if pt[1] < self.cStopThresh:
         self.above_thresh = False
@@ -68,21 +88,37 @@ class PeakForce():
     for i in range(len(self.plotx)):
       if len(self.ploty) > 0:
         mm = max(self.ploty[i])
-        self.ax.plot(self.plotx[i],self.ploty[i], label="{:.2f} kg".format(mm))
-        self.ax.legend()
+        self.ax.plot(self.plotx[i],self.ploty[i], label="{} {:.2f} kg".format(self.labels[i], mm))
+    if len(self.plotx) > 0:
+      self.ax.legend()
 
-  def sstart(self, e):
+  def sclear(self, e):
+    self.clear()
+  def clear(self):
     self.plotx = []
     self.ploty = []
     self.t0=[]
-    self.collecting = True
+    self.labels=[]
+    self.tstr = datetime.now().strftime("%Y-%m-%d-%H-%M-%S-") + self.hproto
 
   def sstop(self, e):
     self.collecting = False
 
+  def sresume(self, e):
+    self.collecting = True
+  
   def log_force_sample(self, time, weight):
     '''Callback with data update from the progressor'''
     self.dq.append([time,weight])
+
+  def ssave(self, e):
+    fbase=self.basedir + self.tstr
+    self.fig.savefig(fbase + '.svg')
+    for i in range(len(self.plotx)):
+      if len(self.ploty) > 0:
+        fname=fbase+'-'+str(self.t0[i])+'-'+self.labels[i]+".csv"
+        savetxt(fname , [self.plotx[i], self.ploty[i]], delimiter=",")
+        print("saved " + fname)
 
   def start(self):
     self.animator=animation.FuncAnimation(self.fig, self.update_plot, interval=500, blit=False)
